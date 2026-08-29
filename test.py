9975e8e09785aef5a372971b6009b6e20e323d6e
Jianru Lin <jianru.lin@gmail.com>
2026-08-29 20:51:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,19 @@ class TestScriptParser(unittest.TestCase):
     def test_empty_script(self):
         self.assertEqual(parse(b""), [])
 
+    def test_single_and_combined_opcodes(self):
+        raw = bytes([Opcode.OP_DUP, Opcode.OP_HASH160, Opcode.OP_EQUALVERIFY, Opcode.OP_CHECKSIG])
+        expected = [
+            Token(Opcode.OP_DUP),
+            Token(Opcode.OP_HASH160),
+            Token(Opcode.OP_EQUALVERIFY),
+            Token(Opcode.OP_CHECKSIG),
+        ]
+        self.assertEqual(parse(raw), expected)
+
+    def test_direct_pushdata_small(self):
+        raw_4bytes = b"\x04\xde\xad\xbe\xef"
+        self.assertEqual(parse(raw_4bytes), [Token(Opcode.OP_PUSHDATA, b"\xde\xad\xbe\xef")])
+
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
