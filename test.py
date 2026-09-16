42d406eddcd51f9264f50c8b038fa94750519afc
Jianru Lin <jianru.lin@gmail.com>
2026-09-16 17:03:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -146,6 +146,11 @@ class TestScriptStack(unittest.TestCase):
         self.assertEqual(stack.pop(), b"\x01")
         self.assertEqual(len(stack), 0)
 
+    def test_peek_empty(self):
+        stack = ScriptStack()
+        with self.assertRaises(ValueError):
+            _ = stack.peek(0)
+
 
 if __name__ == "__main__":
     _ = unittest.main(verbosity=2)
